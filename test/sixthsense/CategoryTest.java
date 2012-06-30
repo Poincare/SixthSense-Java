bf4b35d8478b59a1830806627a8e601966f5dac6
Aroop Ganguly <aroopganguly@gmail.com>
2012-06-30 08:22:05 -0400

    applied the code styling policy to CategoryTest and renamed the testcases for constructors to meaningful names.

--- test/sixthsense/CategoryTest.java
+++ test/sixthsense/CategoryTest.java
@@ -9,55 +9,68 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 import org.junit.Test;
-public class CategoryTest {
+
+public class CategoryTest
+{
 	@Test
-	public void testConstructor1() {
-		Category c =new Category("c1");
-		if(!c.Name().equals("c1")) {
+	public void testCtorForNameMemberInit()
+	{
+		Category c = new Category("c1");
+		if (!c.Name().equals("c1"))
+		{
 			fail("Category constructor Name value incorrect");
 		}
 	}
-	
+
 	@Test
-	public void testConstructor2() {
+	public void testCtorForNameAndObjectMembersInit()
+	{
 		Category c = null;
-		try {
-			c = new Category("c1",new Gesture());
-		} catch (Exception e) {
+		try
+		{
+			c = new Category("c1", new Gesture());
+		}
+		catch (Exception e)
+		{
 			fail("failed to add example, " + e.getMessage());
 		}
-		if(c==null || c.Examples()==0) {
+		if (c == null || c.Examples() == 0)
+		{
 			fail("no examples added in constructor");
 		}
-		else if(c.Examples()>1)
+		else if (c.Examples() > 1)
 		{
 			fail("garbge values for examples inside Category");
 		}
 	}
-	
+
 	@Test
-	public void testConstructor3() {
+	public void testCtorForGenericParamsInit()
+	{
 		Category c = null;
-		ArrayList<String> data =new ArrayList<String>(5);
-		for(String s:data)
+		ArrayList<String> data = new ArrayList<String>(5);
+		for (@SuppressWarnings("unused") String s : data)
 		{
-			s="t";
+			s = "t";
 		}
-		try {
-			
-			c = new Category("c1",data);
-		} catch (Exception e) {
+		try
+		{
+			c = new Category("c1", data);
+		}
+		catch (Exception e)
+		{
 			fail("failed to add example, " + e.getMessage());
 		}
-		
-		if(c==null || c.Examples()!=data.size()) {
+
+		if (c == null || c.Examples() != data.size())
+		{
 			fail("no examples added incorrectly in constructor");
 		}
-		else 
+		else
 		{
-			for(int i=0; i<5;i++)
+			for (int i = 0; i < 5; i++)
 			{
-				if(!c.getGesture(i).getClass().equals(Gesture.class))
+				if (!c.getGesture(i).getClass().equals(Gesture.class))
 				{
 					fail("Examples not added properly as Gestures in Contructor");
 					break;
@@ -65,60 +78,60 @@ public class CategoryTest {
 			}
 		}
 	}
-	
+
 	@Test
 	public void testName()
 	{
 		Category c = new Category("test");
-		if(!c.Name().equals("test"))
+		if (!c.Name().equals("test"))
 		{
 			fail("Name not retrived correctly");
 		}
 	}
-	
+
 	@Test
 	public void testExample()
 	{
-		
-		ArrayList<String> data =new ArrayList<String>(5);
-		for(@SuppressWarnings("unused") String s:data)
+
+		ArrayList<String> data = new ArrayList<String>(5);
+		for (@SuppressWarnings("unused")
+		String s : data)
 		{
-			s="t";
+			s = "t";
 		}
-		
-		Category c = new Category("test",data);
-		if(!(c.Examples()==data.size()))
+
+		Category c = new Category("test", data);
+		if (!(c.Examples() == data.size()))
 		{
 			fail("Example() not working properly");
 		}
 	}
-	
+
 	@Test
 	public void testAddExample()
 	{
 		Category c = new Category("test");
-		try{
+		try
+		{
 			c.AddExample(new Gesture());
 		}
-		catch(Exception e)
+		catch (Exception e)
 		{
 			fail("error in adding examples." + e.getMessage());
 		}
-		if(c.Examples()!=1)
+		if (c.Examples() != 1)
 		{
 			fail("error in adding examples");
 		}
 	}
-	
-	@Test 
+
+	@Test
 	public void testParseName()
 	{
-		String testStr="abc1bcd";
-		if(Category.ParseName(testStr)!="abc1b")
+		String testStr = "abc1bcd";
+		if (Category.ParseName(testStr) != "abc1b")
 		{
 			fail("ParseName is incorrect");
 		}
 	}
-	
-	
 }
