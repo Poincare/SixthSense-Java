e58c6f54418405f9afcc50738c72a143cd93d4bd
Dhaivat Pandya <dhaivatpandya@gmail.com>
2012-07-01 11:34:47 -0500

    Removed gradle, added comments on RectangleR and PointR

--- src/sixthsense/RectangleR.java
+++ src/sixthsense/RectangleR.java
@@ -6,6 +6,10 @@ package sixthsense;
 
 import java.util.List;
 
+/* RectangleR:
+ * 
+ * A utility class that basically implements the idea of a rectangle.
+ */
 public class RectangleR {
 	//digits to which measurements are rounded
 	private int digits = 4;
@@ -18,6 +22,7 @@ public class RectangleR {
 	private double width;
 	private double height;
 	
+	//constructor to copy values over
 	public RectangleR(double X, double Y, double W, double H) {
 		x = X;
 		y = Y;
@@ -25,30 +30,37 @@ public class RectangleR {
 		height = H;
 	}
 	
+	//get x value
 	public double getX() {
 		return x;
 	}
 	
+	//get y value
 	public double getY() {
 		return y;
 	}
 	
+	//get width of the RectangleR
 	public double getWidth() {
 		return width;
 	}
 	
+	//get height of the RectangleR
 	public double getHeight() {
 		return height;
 	}
 	
+	//get topleft corner
 	public PointR getTopleft() {
 		return new PointR(x, y);
 	}
 	
+	//get bottomright corner
 	public PointR getBottomRight() {
 		return new PointR(x+width, y+height);
 	}
 	
+	//get center/centroid of the rectangle
 	public PointR getCenter() {
 		double center_x = x+width/2;
 		double center_y = y+height/2;
@@ -58,18 +70,22 @@ public class RectangleR {
 		return res;
 	}
 	
+	//get the length of the longest side of the rectangle
 	public double getMaxSide() {
 		return Math.max(width, height);
 	}
 
+	//get the length of the shortest side of the rectangle
 	public double getMinSide() {
 		return Math.min(width, height);
 	}
 
+	//get the length of the diagonal of the rectangle
 	public double getDiagonal() {
 		return PointR.distance(getTopleft(), getBottomRight());
 	}
 	
+	//basic equals override - checks equality of everything, not just if translated
 	public boolean equals(Object b) {
 		if(b instanceof RectangleR) {
 			RectangleR c = (RectangleR)b;
@@ -80,6 +96,7 @@ public class RectangleR {
 		return false;
 	}
 	
+	//Find the largest box with the specified points
 	public static RectangleR findBox(List<PointR> points) {
 		double maxX = Double.MIN_VALUE;
 		double maxY = Double.MIN_VALUE;
@@ -108,6 +125,7 @@ public class RectangleR {
 		return new RectangleR(minX, minY, maxX-minX, maxY-minY);
 	}
 
+	//getter-setter -- Mostly from legacy C# code
 	public int getDigits() {
 		return digits;
 	}
