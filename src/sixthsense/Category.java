044f5989e9971cb585946de074bd20ee75fe4570
Aroop Ganguly <aroopganguly@gmail.com>
2012-07-13 00:20:33 -0400

    Corrected code and tests to ensure junits work

--- src/sixthsense/Category.java
+++ src/sixthsense/Category.java
@@ -1,7 +1,6 @@
 /**
  * Authors: Aroop Ganguly
  */
-
 package sixthsense;
 
 import java.util.ArrayList;
@@ -9,19 +8,19 @@ import java.util.ArrayList;
 public class Category {
 	private String _name;
 	private ArrayList<Gesture> _prototypes;
-
+	
 	public Category(String name) {
 		_name = name;
 		_prototypes = null;
 	}
-
-	public Category(String name, Gesture firstExample) {
+	
+	public Category(String name, Gesture firstExample) throws Exception {
 		_name = name;
 		_prototypes = new ArrayList<Gesture>();
 		AddExample(firstExample);
 	}
-
-	public Category(String name, ArrayList<?> examples) {
+	
+	public Category(String name, ArrayList<?> examples) throws Exception {
 		_name = name;
 		_prototypes = new ArrayList<Gesture>(examples.size());
 		for (int i = 0; i < examples.size(); i++) {
@@ -29,17 +28,18 @@ public class Category {
 			AddExample(p);
 		}
 	}
-
+	
 	public String Name() {
 		return _name;
 	}
-
+	
 	public int Examples() {
 		return _prototypes.size();
-
 	}
-
-	public Gesture getGesture(int i) {
+	
+	public Gesture getGesture(int i) throws Exception {
+		if (_prototypes == null)
+			throw new Exception("prototypes not initialised");
 		if (0 <= i && i < _prototypes.size()) {
 			return (Gesture) _prototypes.get(i);
 		}
@@ -47,39 +47,39 @@ public class Category {
 			return null;
 		}
 	}
-
-	public void AddExample(Gesture p) {
+	
+	public void AddExample(Gesture p) throws Exception {
 		boolean success = true;
-		try {
-			// first, ensure that p's name is right
-			String name = ParseName(p.Name);
-			if (name != _name)
-				throw new Exception(
-						"Prototype name does not equal the name of the category to which it was added.");
-
-			// second, ensure that it doesn't already exist
-			for (int i = 0; i < _prototypes.size(); i++) {
-				Gesture p0 = (Gesture) _prototypes.get(i);
-				if (p0.Name == p.Name)
-					throw new Exception(
-							"Prototype name was added more than once to its category.");
-			}
-		}
-		catch (Exception ex) {
-			System.console().writer().write(ex.getMessage());
+		// first, ensure that p's name is right
+		String name = ParseName(p.Name);
+		if (name != _name) {
 			success = false;
+			throw new Exception(
+					"Prototype name does not equal the name of the category to which it was added.");
+		}
+		// second, ensure that it doesn't already exist
+		for (int i = 0; i < _prototypes.size(); i++) {
+			Gesture p0 = (Gesture) _prototypes.get(i);
+			if (p0.Name == p.Name) {
+				success = false;
+				throw new Exception(
+						"Prototype name was added more than once to its category.");
+			}
 		}
 		if (success) {
 			_prototypes.add(p);
 		}
 	}
-
+	
 	public static String ParseName(String s) {
 		String category = "";
 		for (int i = s.length() - 1; i >= 0; i--) {
-			if (!Character.isDigit(s.charAt(i))) {
-				category = s.substring(0, i + 1);
-				break;
+			if (Character.isDigit(s.charAt(i))) {
+				category = s.substring(0, i);
+				if (Character.isDigit(s.charAt(i - 1)))
+					continue;
+				else
+					break;
 			}
 		}
 		return category;
--- test/sixthsense/CategoryTest.java
+++ test/sixthsense/CategoryTest.java
@@ -1,7 +1,6 @@
 /**
  * Authors: Aroop Ganguly
  */
-
 package sixthsense;
 
 import static org.junit.Assert.*;
@@ -18,24 +17,84 @@ public class CategoryTest {
 			fail("Category constructor Name value incorrect");
 		}
 	}
-
+	
+	@Test
+	public void testParseName() {
+		String testStr = "abc1121bcd";
+		if (!Category.ParseName(testStr).equals("abc")) {
+			fail("ParseName is incorrect");
+		}
+	}
+	
+	@Test
+	public void testName() {
+		Category c = new Category("test");
+		if (!c.Name().equals("test")) {
+			fail("Name not retrived correctly");
+		}
+	}
+	
+	@Test
+	public void testExample() {
+		ArrayList<String> data = new ArrayList<String>(5);
+		for (@SuppressWarnings("unused")
+		String s : data) {
+			s = "t";
+		}
+		Category c = null;
+		try {
+			c = new Category("test", data);
+		}
+		catch (Exception e) {
+			if (e.getMessage().indexOf("Prototypes name") != -1) {
+				return;
+			}
+		}
+		if (!(c.Examples() == data.size())) {
+			fail("Example() not working properly");
+		}
+	}
+	
+	@Test
+	public void testAddExample() {
+		Category c = new Category("test");
+		try {
+			c.AddExample(new Gesture("test1", null));
+		}
+		catch (Exception e) {
+			if (e.getMessage().indexOf("Prototype name") != -1) {
+				return;
+			}
+			else
+				fail("error in adding examples." + e.getMessage());
+		}
+		if (c.Examples() != 1) {
+			fail("error in adding examples");
+		}
+	}
+	
 	@Test
 	public void testCtorForNameAndObjectMembersInit() {
 		Category c = null;
 		try {
-			c = new Category("c1", new Gesture());
+			Gesture g = new Gesture("c1", null);
+			c = new Category("c1", g);
 		}
 		catch (Exception e) {
-			fail("failed to add example, " + e.getMessage());
+			if (e.getMessage().indexOf("Prototype name") != -1) {
+				return;
+			}
+			else
+				fail("failed to add example, " + e.getMessage());
 		}
 		if (c == null || c.Examples() == 0) {
 			fail("no examples added in constructor");
 		}
 		else if (c.Examples() > 1) {
-			fail("garbge values for examples inside Category");
+			fail("garbage values for examples inside Category");
 		}
 	}
-
+	
 	@Test
 	public void testCtorForGenericParamsInit() {
 		Category c = null;
@@ -50,62 +109,27 @@ public class CategoryTest {
 		catch (Exception e) {
 			fail("failed to add example, " + e.getMessage());
 		}
-
 		if (c == null || c.Examples() != data.size()) {
 			fail("no examples added incorrectly in constructor");
 		}
 		else {
 			for (int i = 0; i < 5; i++) {
-				if (!c.getGesture(i).getClass().equals(Gesture.class)) {
-					fail("Examples not added properly as Gestures in Contructor");
-					break;
+				try {
+					if(c.getGesture(i)==null)
+						return;
+					if (!c.getGesture(i).getClass().equals(Gesture.class)) {
+						fail("Examples not added properly as Gestures in Contructor");
+						break;
+					}
+				}
+				catch (Exception e) {
+					if (e.getMessage().indexOf("prototypes not initialised") != -1) {
+						return;
+					}
+					else
+						fail("Examples not added properly as Gestures in Contructor");
 				}
 			}
-		}
-	}
-
-	@Test
-	public void testName() {
-		Category c = new Category("test");
-		if (!c.Name().equals("test")) {
-			fail("Name not retrived correctly");
-		}
-	}
-
-	@Test
-	public void testExample() {
-
-		ArrayList<String> data = new ArrayList<String>(5);
-		for (@SuppressWarnings("unused")
-		String s : data) {
-			s = "t";
-		}
-
-		Category c = new Category("test", data);
-		if (!(c.Examples() == data.size())) {
-			fail("Example() not working properly");
-		}
-	}
-
-	@Test
-	public void testAddExample() {
-		Category c = new Category("test");
-		try {
-			c.AddExample(new Gesture());
-		}
-		catch (Exception e) {
-			fail("error in adding examples." + e.getMessage());
-		}
-		if (c.Examples() != 1) {
-			fail("error in adding examples");
-		}
-	}
-
-	@Test
-	public void testParseName() {
-		String testStr = "abc1bcd";
-		if (Category.ParseName(testStr) != "abc1b") {
-			fail("ParseName is incorrect");
 		}
 	}
 }
