5f6d6f77ba02e0d2f183170dd7d72457427f3a98
Romke van der Meulen <redge.online@gmail.com>
2012-09-08 22:09:12 +0200

    Fixed some warnings that Eclipse detected
     * Category class: dropped unused variable success from addExample()
     * PointR class: dropped unused variable r from scaleBy()
     * GestureTest class: dropped unused import java.util.List
     * Added static long serialVersionUID to both Exception classes

--- src/sixthsense/NBestListEmptyException.java
+++ src/sixthsense/NBestListEmptyException.java
@@ -1,6 +1,9 @@
 package sixthsense;
 
 public class NBestListEmptyException extends Exception {
+	
+	static final long serialVersionUID = 0;
+	
 	public String getMessage() {
 		return "NBestList passed is empty";
 	}
--- src/sixthsense/NotEnoughPointsException.java
+++ src/sixthsense/NotEnoughPointsException.java
@@ -1,6 +1,9 @@
 package sixthsense;
 
 public class NotEnoughPointsException extends Exception {
+	
+	static final long serialVersionUID = 0;
+	
 	public String getMessage() {
 		return "there aren't enough points in the given list";
 	}
