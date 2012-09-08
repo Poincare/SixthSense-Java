3d34e4f033042a7fdc948c55509bb2ad8ab5acc4
Romke van der Meulen <redge.online@gmail.com>
2012-09-09 01:37:14 +0200

    Removed serialVersionUID from Exception classes

--- src/sixthsense/NBestListEmptyException.java
+++ src/sixthsense/NBestListEmptyException.java
@@ -2,8 +2,6 @@ package sixthsense;
 
 public class NBestListEmptyException extends Exception {
 	
-	static final long serialVersionUID = 0;
-	
 	public String getMessage() {
 		return "NBestList passed is empty";
 	}
--- src/sixthsense/NotEnoughPointsException.java
+++ src/sixthsense/NotEnoughPointsException.java
@@ -2,8 +2,6 @@ package sixthsense;
 
 public class NotEnoughPointsException extends Exception {
 	
-	static final long serialVersionUID = 0;
-	
 	public String getMessage() {
 		return "there aren't enough points in the given list";
 	}
